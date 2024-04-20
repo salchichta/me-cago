3b4ba63223ec9fc6b52173331018ce9a11c40ac4
salchichta <162981450+salchichta@users.noreply.github.com>
2024-04-20 13:06:16 +0200

    Add files via upload

---
+++ me cago.py
@@ -0,0 +1,7 @@
+quye miras bobo?
+def eres_tonto(iq,edad):
+    if iq < 100 and edad > 5:
+        print("eres puto gilipoollas")
+    else:
+        print("que, Quieres un premio Bribon?")
+        
